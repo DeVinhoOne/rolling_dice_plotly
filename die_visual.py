548f4e8b7adc655a01b6e35852d5041e4a116c36
Dawid Lacheta <caushered@gmail.com>
2020-07-17 09:15:26 +0200

    update .gitignore

--- die_visual.py
+++ die_visual.py
@@ -4,27 +4,29 @@ from plotly import offline
 from die import Die
 
 
-# Create a D6.
+# Create one D6 die.
 die = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
+num_of_rolls = 1000
+for roll_num in range(num_of_rolls):
     result = die.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': 'Results'}
+x_axis_config = {'title': 'Results', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Results'}
-my_layout = Layout(title='Results of rolling one D6 1000 times',
+my_layout = Layout(title=f'Results of rolling one D6 {num_of_rolls} times',
                 xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
